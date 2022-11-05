64c9ac9d7d735dc10ee3f2909484cdebd78962ff
Nik Mel <you@example.com>
2022-11-05 19:30:16 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -18,11 +21,19 @@ export default {
 
 <style>
 #app {
+  top: 1px;
+  left: 1px;
+
+  width: 1px;
+  height: 1px;
+  margin-top: 60px;
+
+  text-align: center;
+
+  color: #2c3e50;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
