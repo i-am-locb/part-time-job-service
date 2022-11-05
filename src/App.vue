80feb3c9ae8fb3bb081ad7251b160abac4863c42
Nik Mel <you@example.com>
2022-11-05 21:22:42 +0300

    testing fake api

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,67 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="post in posts" :key="post.id">
+      <p>ID: {{post.id}} </p><br>
+      <p>Title: {{post.title}} </p><br>
+      <p>Author: {{post.author}} </p><br>
+    </div>
+    <div v-for="comment in comments" :key="comment.id">
+      <p>ID: {{comment.id}} </p><br>
+      <p>Body: {{comment.body}} </p><br>
+      <p>PostId: {{comment.postId}} </p><br>
+    </div>
+    <p>Name {{profile.name}}</p>
+    <div>
+      <input type="text" v-model="todoName" @keyup.enter="addTodo">
+      <ul>
+        <li v-for="todo, index in todos" :key="index"> {{todo}} </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      posts: [],
+      comments: [],
+      profile: [],
+      todos: [],
+      todoName: ''
+    }
+  },
+  async created () {
+    try {
+      const posts = await axios.get('http://localhost:3000/posts')
+      const comments = await axios.get('http://localhost:3000/comments')
+      const profile = await axios.get('http://localhost:3000/profile')
+      const todos = await axios.get('http://localhost:3000/todos')
+      this.posts = posts.data
+      this.comments = comments.data
+      this.profile = profile.data
+      this.todos = todos.data
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  methods: {
+    async addTodo () {
+      const res = await axios.post('http://localhost:3000/todos', { name: this.todoName })
+      this.todos = [...this.todos, res.data]
+      this.todoName = ''
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  top: 1px;
-  left: 1px;
-
-  width: 1px;
-  height: 1px;
   margin-top: 60px;
 
   text-align: center;
